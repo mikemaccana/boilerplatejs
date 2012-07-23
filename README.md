0d0395be2adfd5fd4e9395dfa8317dc66701cd7d
Mike MacCana <mike.maccana@bazaarvoice.com>
2012-07-23 19:33:19 +0100

    Tags, artwork, layout.

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
-# boilerplate.js
+# Boy Meets Girl Meets Toaster
 
-A template for full-stack Javascript apps
-
- * A filled out package.json
- * Shared code between the server and the browser out of the box
- * Async, script-tag free JS looading on the client
- * A config file, with common related utilities to prompt for passwords and expand home dirs
- * Mustache templating, LESS.CSS
- 
-License MIT
-
-### Installing Boilerplate JS
-
-Assuming you've already got node installed, run:
-
-	npm install .
+The Prince’s Trust is a registered charity number 1079675, incorporated by Royal Charter.
 
 ### What's with this 'less' stuff?
 
@@ -22,3 +8,4 @@ boilerplate.js uses less for CSS variables etc. You can regenerate the .CSS file
     
     npm install -g less
     lessc -x public/less/boilerplate.less > public/css/boilerplate.css 
+http://www.princes-trust.org.uk/support_us/fundraise/support_and_advice/publicity_and_marketing.aspx
--- app.js
+++ app.js
@@ -29,7 +29,8 @@ requirejs(['express', 'utils', './public/js/lib/shared.js',], function (express,
       author:'Jane',
       location:'Lancashire',
       link:'http://direct.asda.com/Nokia-100-Mobile-Phone---Vodafone/008718298,default,pd.html',
-      content:'This is the phone for you if you really do just want a <h4>decent basic mobile phone</h4> rather than a <h6>portable office/</h6><h5>music studio/</h5><h5>cinema/</h5><h4>British library/</h4><h5>missile tracking system/</h5><h4>hadron collider/</h4><h6>bat detector/</h6><h5>trading floor/</h5><h5>muffin tin/<h5></h6>paperweight</h6>.'
+      content:'This is the phone for you if you really do just want a <h4>decent basic mobile phone</h4> rather than a <h6>portable office/</h6><h5>music studio/</h5><h5>cinema/</h5><h4>British library/</h4><h5>missile tracking system/</h5><h4>hadron collider/</h4><h6>bat detector/</h6><h5>trading floor/</h5><h5>muffin tin/<h5></h6>paperweight</h6>.',
+      tags:['love','bizarre']
     }
     utils.respondWithTemplate(response, 'hello', template_data)
   });
