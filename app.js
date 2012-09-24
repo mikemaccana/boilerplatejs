19fb6a3247579f9e49ebe5cdc7600a9686d3e4a2
Mike MacCana <mike.maccana@gmail.com>
2012-09-24 19:55:23 +0100

    Working partial support.

--- app.js
+++ app.js
@@ -5,11 +5,14 @@
 var requirejs = require('requirejs');
 requirejs.config({ nodeRequire: require, baseUrl: "lib" });
 
-requirejs(['express', 'utils', 'fs', './public/js/lib/agave.js'], function (express, utils, fs, agave) {
+requirejs(['express', 'utils', 'fs', 'hulk-hogan', './public/js/lib/agave.js'], function (express, utils, fs, hulk,agave) {
   
-  var REVIEWS_FILE = 'reviews.json'
+  var REVIEWS_FILE = 'reviews.json';
+  
+  var PORT = 3000;
   
   var app = express.createServer();
+  
   app.configure(function(){
     app.use(express.methodOverride()); // Allow browsers to simulate PUT etc.
     app.use(express.bodyParser()); // Support POST form bodies. Why would someone ever *not* want this?
@@ -19,7 +22,7 @@ requirejs(['express', 'utils', 'fs', './public/js/lib/agave.js'], function (expr
       dumpExceptions: true, 
       showStack: true
     }));
-    app.use(app.router);
+    app.use(app.router);  
   });
   
   // Load our reviews
@@ -28,26 +31,28 @@ requirejs(['express', 'utils', 'fs', './public/js/lib/agave.js'], function (expr
 
     // Overall view
     app.get('/', function(request, response){
-      console.log('Server running on port 3000')
-      utils.respondWithTemplate(response, 'site', reviews[0])
+      console.log('Visit to /')
+      utils.respondWithTemplate(response, 'site', {reviews:reviews}, {reviewtemplate:'review'})
     });    
   
     // Individual page view
-    app.get("/:review_slug", function(req, res) {    
+    app.get("/reviews/:review_slug", function(req, res) {    
+     console.log('Visit to /reviews/')
      var review_slug = req.params.review_slug;
      var review = reviews.findItem(function(item){
        return item.slug = review_slug;
      })     
-     utils.respondWithTemplate(response, 'site', review)
+     utils.respondWithTemplate(response, 'site', {reviews:[review]}, {reviewtemplate:review})
     })
      
     // Admin view
     app.get('/admin', function(request, response){
+      console.log('Visit to admin')
       response.end('admin goes here');
     })
-
-
-    app.listen(3000);    
+    
+    console.log('Server running on port '+3000)
+    app.listen(PORT);    
   })
   
 
