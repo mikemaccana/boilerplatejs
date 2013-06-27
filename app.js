805de8f9bd446eb53eef285c0e71a98580d7cfe0
Mike MacCana <mike.maccana@gmail.com>
2013-06-27 14:39:50 +0100

    Remove some accidentally committed code from a charity project.

--- app.js
+++ app.js
@@ -20,14 +20,8 @@ requirejs(['express', 'utils', './public/js/lib/shared.js',], function (express,
   app.get('/', function(request, response){
     // response.send('Hello World');
     var template_data = {
-      title:'Boy Meets Girl Meets Toaster',
-      vendor:'Asda',
-      date:'2012-07-09 04:28:54',
-      item:'Nokia 100 Mobile Phone - Vodafone',
-      author:'Jane',
-      location:'Lancashire',
-      link:'http://direct.asda.com/Nokia-100-Mobile-Phone---Vodafone/008718298,default,pd.html',
-      content:'This is the phone for you if you really do just want a <h4>decent basic mobile phone</h4> rather than a <h6>portable office/</h6><h5>music studio/</h5><h5>cinema/</h5><h4>British library/</h4><h5>missile tracking system/</h5><h4>hadron collider/</h4><h6>bat detector/</h6><h5>trading floor/</h5><h5>muffin tin/<h5></h6>paperweight</h6>.'
+      title:'Boilerplate JS',
+      content:'This is a sample web page.'
     }
     utils.respondWithTemplate(response, 'hello', template_data)
   });
