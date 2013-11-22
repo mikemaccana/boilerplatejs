f5e6ba25781c42cc0855fccbee06e3ea7f68737d
Mike MacCana <mike.maccana@gmail.com>
2013-11-22 23:09:40 +0000

    Move from hogan to express handlebars

--- server.js
+++ server.js
@@ -3,14 +3,27 @@
 var requirejs = require('requirejs');
 requirejs.config({ nodeRequire: require, baseUrl: "lib" });
 
-requirejs(['express', 'utils', './public/js/lib/shared.js',], function (express, utils, shared) {
-  var PORT = 3000;
+requirejs(['express', 'express3-handlebars', 'utils', './public/js/lib/shared.js'],
+  function (express, expressHandlebars, utils, shared) {
+
+  var PORT = process.env.PORT || 3000;
   var app = express();
+
+  global.log = console.log.bind(console); // Save some typing
+
+  // use handlebars templating
+  app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main' // This layout is the html5 layout
+  }));
+  app.enable('view cache'); // Cache templates
+
   app.configure(function(){
+    app.set('port', PORT);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'handlebars');
     app.use(express.methodOverride()); // Allow browsers to simulate PUT etc.
     app.use(express.bodyParser()); // Support POST form bodies. Why would someone ever *not* want this?
-    // 'express.static' causes jshint warning (static is reserved word).
-    app.use('/public', express['static']('public'));
+    app.use('/public', express.static('public'));
     app.use(express.errorHandler({
       dumpExceptions: true,
       showStack: true
@@ -20,11 +33,11 @@ requirejs(['express', 'utils', './public/js/lib/shared.js',], function (express,
 
   app.get('/', function(request, response){
     // response.send('Hello World');
-    var template_data = {
+    var templateData = {
       title:'Boilerplate JS',
       content:'This is a sample web page.'
     }
-    utils.respondWithTemplate(response, 'hello', template_data)
+    utils.respondWithTemplate(response, 'hello', {}, templateData)
   });
 
   app.listen(PORT);
