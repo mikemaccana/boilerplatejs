961351d2878fb146ec570ccb65b5c0b8ecb098ad
Mike MacCana <mike.maccana@gmail.com>
2013-11-22 23:11:04 +0000

    Use grunt autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,15 +15,28 @@ module.exports = function(grunt) {
           yuicompress: false
         },
         files: {
-          "./public/css/firework.css": "./public/less/firework.less"
+          "./public/css/boilerplate.css": "./public/less/boilerplate.less"
         }
+      }
+    },
+
+    // Automatically add prefixes for browsers
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version', 'ie 9']
+      },
+      development: {
+        expand: true,
+        flatten: true,
+        src: 'public/css/*.css',
+        dest: 'public/css'
       }
     },
 
     watch: {
       less: {
         files: ["./public/less/*"],
-        tasks: ["less"],
+        tasks: ["less", "autoprefixer:development"],
         options: {
           livereload: true
         }
